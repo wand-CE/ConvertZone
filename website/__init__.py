1acbaef0ef4edf0aeafab1171580252fa13f3ef5
wand-CE <wanderson14117@gmail.com>
2023-09-08 01:48:49 -0400

    added big changes on the project config

--- website/__init__.py
+++ website/__init__.py
@@ -1,21 +1,35 @@
 import os
+import time
+from datetime import timedelta
+import schedule
 from flask import Flask
-from sqlalchemy import create_engine, inspect
 from flask_sqlalchemy import SQLAlchemy
+
 
 db = SQLAlchemy()
 DB_NAME = 'convert_zone.db'
 
 
 def create_app():
+    from .auth import auth
     from .views import views
+    from .http_requests import http_requests
+
     app = Flask(__name__)
 
+    app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(http_requests, url_prefix='/api')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.secret_key = 'your secret'
+    app.permanent_session_lifetime = timedelta(minutes=30)
+
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    app.config['MEDIA'] = os.path.join(BASE_DIR, 'media')
 
     db.init_app(app)
+
+    start_cleanup_thread(app)
 
     return app
 
@@ -23,6 +37,46 @@ def create_app():
 def create_tables(app):
     with app.app_context():
         db.create_all()
-        # from website.models.servicos_model import insert_function_names
-        # insert_function_names()
-        # remove above comment if there's more functions to add to the site
+        from website.models.servicos_model import insert_function_names
+        insert_function_names()
+
+
+def delete_old_folders(path_to_check, age_limit_seconds):
+    """
+    Delete folders that are older than age_limit_seconds.
+    """
+    import shutil
+
+    current_time = time.time()
+
+    for foldername in os.listdir(path_to_check):
+        folder_path = os.path.join(path_to_check, foldername)
+
+        if os.path.isdir(folder_path):
+            creation_time = os.path.getctime(folder_path)
+
+            if current_time - creation_time > age_limit_seconds:
+                # Remove the folder if it's old enough
+                try:
+                    shutil.rmtree(folder_path)
+                except Exception as e:
+                    pass
+
+
+def run_scheduled_cleanup(app):
+    age_limit_seconds = 1860
+    path = os.path.join(app.config['MEDIA'], 'temp_users')
+    schedule.every(5).minutes.do(delete_old_folders,
+                                 path, age_limit_seconds)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+def start_cleanup_thread(app):
+    import threading
+
+    thread = threading.Thread(target=run_scheduled_cleanup, args=(app,))
+    thread.daemon = True
+    thread.start()
